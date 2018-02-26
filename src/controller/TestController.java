aba51c9b1c840f2346aa992192d3f6a95744dbb3
lujialei <lujialei08@163.com>
2018-02-26 15:03:25 +0800

    配置数据库

---
+++ src/controller/TestController.java
@@ -0,0 +1,18 @@
+package controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@RequestMapping(value = "/test")
+@Controller
+public class TestController  {
+
+    @RequestMapping(method = RequestMethod.GET)
+    @ResponseBody
+    public String test(){
+        return "hello lujialei";
+    }
+}
